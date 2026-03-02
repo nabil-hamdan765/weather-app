840aca1d4becfba1d8a8622ce5a491afb3f98a8f
Nabil Hamdan <nabil.hamdan765@gmail.com>
2026-03-02 01:14:18 +0000

    Updated file

--- forge.config.js
+++ forge.config.js
@@ -8,24 +8,6 @@ module.exports = {
   },
 
   rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
